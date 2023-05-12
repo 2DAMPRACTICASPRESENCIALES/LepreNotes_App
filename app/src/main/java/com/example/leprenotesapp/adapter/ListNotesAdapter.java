5553dbca9ef901492c5a2c53da517973e98d7dbe
Juan Senen <juansenen@hotmail.com>
2023-05-12 16:09:02 +0200

    First Adapter List Notes

---
+++ app/src/main/java/com/example/leprenotesapp/adapter/ListNotesAdapter.java
@@ -0,0 +1,62 @@
+package com.example.leprenotesapp.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.leprenotesapp.R;
+import com.example.leprenotesapp.domain.Notes;
+import java.util.List;
+
+public class ListNotesAdapter extends RecyclerView.Adapter<ListNotesAdapter.ListNotesHolder> {
+
+    private List<Notes> notesList;
+    private Context context;
+
+    public ListNotesAdapter (Context context, List<Notes> notesList) {
+        this.context = context;
+        this.notesList = notesList;
+
+    }
+    public Context getContext() {
+        return context;
+    }
+
+    @Override
+    public ListNotesHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.activity_main_item, parent, false);
+        return new ListNotesHolder(view);
+    }
+    @Override
+    public void onBindViewHolder(ListNotesHolder holder, int position) {
+       // holder.codeLine.setText(linesList.get(position).getCodeLine());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return notesList.size();
+    }
+
+    public class ListNotesHolder extends RecyclerView.ViewHolder {
+
+        public TextView title;
+        public View parentView;
+
+        public ListNotesHolder(View view) {
+            super(view);
+            parentView = view;
+
+            title = view.findViewById(R.id.main_rcview_title);
+
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/leprenotesapp/view/MainActivity.java
+++ app/src/main/java/com/example/leprenotesapp/view/MainActivity.java
@@ -1,16 +1,37 @@
 package com.example.leprenotesapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.leprenotesapp.R;
+import com.example.leprenotesapp.adapter.ListNotesAdapter;
+import com.example.leprenotesapp.domain.Notes;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<Notes> noteList;
+    private ListNotesAdapter listNotesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_view);
     }
+    private void initializeRecyclerView() {
+        noteList = new ArrayList<>();
+
+        RecyclerView recyclerView = findViewById(R.id.main_reciclerview);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        listNotesAdapter = new ListNotesAdapter(this, noteList);
+        recyclerView.setAdapter(listNotesAdapter);
+    }
+
 }
